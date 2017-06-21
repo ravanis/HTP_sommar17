5cefacddd923ca9919eb07476b9e9b1c16203759
Andrea Nygren <Andrea Nygren>
2017-06-21 13:59:54 +0200

    changed quality indicators

--- Evaluation/quality_indicators/quality_indicators.m
+++ Evaluation/quality_indicators/quality_indicators.m
@@ -1,4 +1,4 @@
-function quality_indicators(modelType, freq)
+function quality_indicators(modelType, freq, x)
 % Returns quality indicators HTQ, TC25, SARmaxTum
 
 addpath Example\1_Efield_example\Scripts\       
@@ -11,26 +11,23 @@ rootpath = [evalpath filesep '..' filesep 'Example'];
 datapath = [rootpath filesep '1_Efield_example' filesep 'Data'];
 
 % Get rho
+if length(freq) == 1
 create_rho_mat(modelType, freq);
+elseif length(freq)>1
+    create_rho_mat(modelType, freq(1));
+end
 
 % Load matrices
 PLD = Yggdrasil.Utils.load([rootpath filesep '1_Efield_results' filesep 'P_' modelType '_' num2str(freq) 'MHz.mat']);
 tissue_mat = Yggdrasil.Utils.load([datapath filesep 'tissue_mat_' modelType '.mat']);
 rho = Yggdrasil.Utils.load([datapath filesep 'rho_' modelType '.mat']);
-if startsWith(modelType, 'duke') == 1
-    tissue_filepath = ([datapath filesep 'df_duke_neck_cst_' num2str(freq) 'MHz.txt']);
-    elseif modelType == 'child'
-        tissue_filepath = ([datapath filesep 'df_chHead_cst_' num2str(freq) 'MHz.txt']);
-    else
-        error('Model not available. Add to quality_indicators.')
-end
 
 % Calculate SAR
 SARMatrix = PLD./rho;
 
 % Get Q.I.
 addpath Evaluation\qualInds
-[HTQ, SARmaxTum, TC]=getHTQ(tissue_mat, SARMatrix, tissue_filepath);
+[HTQ, SARmaxTum, TC]=getHTQ(tissue_mat, SARMatrix, modelType,freq);
 
 disp(['HTQ is ' num2str(HTQ)])
 disp(['TC25 is ' num2str(TC(1))])
