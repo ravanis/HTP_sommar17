4abd50d99ac3dda50518c42a29752b1f48afaeba
New Hopa <newhopa.chalmers@gmail.com>
2017-06-22 11:16:43 +0200

    small change

--- Example/1_Efield_example_adv/Scripts/writeSettings.m
+++ Example/1_Efield_example_adv/Scripts/writeSettings.m
@@ -1,4 +1,4 @@
-function writeSettings( resultpath,settings, settings_time, modelType, freq, goal_power_tumor)
+function writeSettings( resultpath, settings, settings_time, modelType, freq, goal_power_tumor)
 %writeSettings: writes txt-file out of settings that can be used in LabView
 %   INPUT:
 %   resultpath - path to where results are to be stored, not ending with
@@ -10,23 +10,23 @@ function writeSettings( resultpath,settings, settings_time, modelType, freq, goa
 %                   correspoding to freq-vector, 1 if there is only 1 
 %                   frequency.
 %   modelType - string of type of model
-%   freq - frequency vector correstponding to order settings and
+%   freq - frequency vector in MHz correstponding to order settings and
 %           time_settings
 %   goal_power_tumor - goal power of tumor used in optimization, set to 0
 %                      if time reversal is used instead on opt.
 
-if ~goal_power_tumor
-    fileID=fopen([resultpath filesep 'settings_' modelType '_' freq 'MHz_TR.txt'],'w');
-    fprintf(fileID,'%s %d\r\n','frequency:',freq);
-else 
+if nargin == 5
     if length(freq)==1
-        fileID=fopen([resultpath filesep 'settings_' modelType '_' freq 'MHz_GP' goal_power_tumor '.txt'],'w');
+        fileID=fopen([resultpath filesep 'settings_' modelType '_' num2str(freq) 'MHz_TR.txt'],'w');
         fprintf(fileID,'%s %d\r\n','frequency:',freq);
     else
-        fileID=fopen([resultpath filesep 'settings_' modelType '_1_' used_freq(1:3) '_2_' used_freq(4:6) 'MHz.txt'],'w');
+        fileID=fopen([resultpath filesep 'settings_' modelType '_1_' num2str(used_freq(1:3)) '_2_' num2str(used_freq(4:6)) 'MHz.txt'],'w');
         fprintf(fileID,'%s %.2f %.2f\r\n','time quota:',settings_time);
         fprintf(fileID,'%s %d %d\r\n','frequencies:', freq);
     end
+else 
+        fileID=fopen([resultpath filesep 'settings_' modelType '_' num2str(freq) 'MHz_GP' num2str(goal_power_tumor) '.txt'],'w');
+        fprintf(fileID,'%s %d\r\n','frequency:',freq);
 end
 
 if length(freq)==1
