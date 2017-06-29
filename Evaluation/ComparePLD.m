07bb1773f9dca166a22256a0fe529d601b779b45
New Hopa <newhopa.chalmers@gmail.com>
2017-06-29 16:49:44 +0200

    changed

--- Evaluation/ComparePLD.m
+++ Evaluation/ComparePLD.m
@@ -1,15 +1,16 @@
 % Plot and calcualte quality indicators
 % Name Matlab field PLD1, CST field PLD2. 
 
-freq=400; % Enter frequency
+freq=450; % Enter frequency
+modelType='duke_tongue';
 
 filename=which('CalculatePLD');
 [scriptpath,~,~] = fileparts(filename);
 tissuePath=[scriptpath,'\Data\df_duke_neck_cst_', num2str(freq),'MHz.txt'];
-load([scriptpath, '\Data\tissue_mat.mat']);
+load([scriptpath, '\Data\tissue_mat_',modelType,'.mat']);
 
-[HTQ1, ~, ~]=getHTQ(tissue_Matrix, PLD1,tissuePath);
-[HTQ2, ~, ~]=getHTQ(tissue_Matrix, PLD2,tissuePath);
+[HTQ1, ~, TC1]=getHTQ(tissue_Matrix, PLD1, modelType);
+[HTQ2, ~, TC2]=getHTQ(tissue_Matrix, PLD2, modelType);
 data = prctile(PLD1(:),linspace(1,100));
 
 % Display quality indicators
